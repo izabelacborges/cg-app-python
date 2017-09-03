9d5ebee4f499a1510441749c37b8143d3798f3f5
izabelacborges <izabelac.borges@gmail.com>
2017-09-03 01:25:17 -0300

    Code skeleton

---
+++ main.py
@@ -0,0 +1,17 @@
+import kivy
+kivy.require("1.10.0")
+
+from kivy.app import App
+from kivy.config import Config
+
+class CGApp:
+    pass
+
+class dda:
+    pass
+
+class bresenham:
+    pass
+
+class parametric:
+    pass
