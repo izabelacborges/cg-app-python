bc883dcad289549f921725bda84f78b9e1221042
izabelacborges <izabelac.borges@gmail.com>
2017-09-03 19:44:59 -0300

    Basic app structure

--- main.py
+++ main.py
@@ -3,15 +3,27 @@ kivy.require("1.10.0")
 
 from kivy.app import App
 from kivy.config import Config
+from kivy.graphics import *
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 
-class CGApp:
+
+class CGraphicsMenu(BoxLayout):
     pass
 
-class dda:
-    pass
+class CGraphicsApp(App):
+    
+    def build(self):
+        return CGraphicsMenu()
 
-class bresenham:
-    pass
+# def dda(self):
+#     pass
 
-class parametric:
-    pass
+# def bresenham(self):
+#     pass
+
+# def parametric(self):
+#     pass
+
+cgApp = CGraphicsApp()
+cgApp.run()
