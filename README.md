7c00a091b2a4c2014941cda5f36c509b4e8f0fdc
izabelacborges <izabelac.borges@gmail.com>
2017-09-12 00:22:49 -0300

    Updated README

--- README.md
+++ README.md
@@ -2,10 +2,31 @@
 
 > This is an assignment for my Computer Graphics class and I'll be implementing the DDA, Bresenham's, Parametric Equation, Circumference, Translation, Scaling, Rotation and Cropping algorithms.
 
-This app is being made with python 3.6.1, turtle and tkinter.
+This app is being made with python 3.6.1, and tkinter.
 
 ## Setup
 
+You should install Python 3.6.x
+
+For Windows OS do it by clicking [here](https://www.python.org/downloads/).
+
+For Linux systems, verify if your system already has python 3.6 installed by typing `python --version` and `python3 --version` on your terminal. If not, do it by folowing the next steps:
+
+```shell
+sudo apt-get update
+sudo apt-get install python3
+```
+
+Verify the installation success by typing `python3 --version` again. Any problems with installation, follow the steps on my python introductory course [here](https://github.com/izabelacborges/python-bootcamp-minicourse/blob/master/setup-install/install-linux.md).
+
 ## Run
+
+Clone my repository to your computer by doing a `git clone`:
+
+```shell
+git clone https://github.com/izabelacborges/cg-app-python.git
+```
+
+And when on the root directory for my project, execute `python3 main.py` on your terminal.
 
 ## Guidelines
--- main.py
+++ main.py
@@ -1,29 +1,228 @@
-import kivy
-kivy.require("1.10.0")
+from tkinter import *
+from math import pow, sqrt
 
-from kivy.app import App
-from kivy.config import Config
-from kivy.graphics import *
-from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
-
-
-class CGraphicsMenu(BoxLayout):
-    pass
-
-class CGraphicsApp(App):
+class graphics_app:
     
-    def build(self):
-        return CGraphicsMenu()
+    # ---------- DRAW LINES ----------
+    
+    def draw_dda(self):
+        
+        x1 = int(self.x1_value.get())
+        y1 = int(self.y1_value.get())
+        x2 = int(self.x2_value.get())
+        y2 = int(self.y2_value.get())
 
-# def dda(self):
-#     pass
+        self.object_lst = [x1, y1, x2, y2]
 
-# def bresenham(self):
-#     pass
+        dx = x2 - x1
+        dy = y2 - y1
 
-# def parametric(self):
-#     pass
+        steps, x, y = 0, x1, y1
 
-cgApp = CGraphicsApp()
-cgApp.run()
+        if abs(dx) > abs(dy):
+            steps = abs(dx)
+        else:
+            steps = abs(dy)
+        
+        x_increment = dx/steps
+        y_increment = dy/steps
+
+        for i in range(steps):
+            x = x + x_increment
+            y = y + y_increment
+
+            self.canvas.create_rectangle(round(x), round(y), round(x)+1, round(y)+1)
+    
+    def draw_bresenham(self):
+        
+        x1 = int(self.x1_value.get())
+        y1 = int(self.y1_value.get())
+        x2 = int(self.x2_value.get())
+        y2 = int(self.y2_value.get())
+
+        self.object_lst = [x1, y1, x2, y2]
+
+        x, y = x1, y1
+        
+        self.canvas.create_rectangle(x, y, x+1, y+1)
+        
+        dx = x2 - x1
+        dy = y2 - y1
+        x_increment, y_increment = 0, 0
+        
+        if dx < 0:
+            dx = dx * -1
+            x_increment = -1
+        else:
+            x_increment = 1
+
+        if dy < 0:
+            dy = dy * -1
+            y_increment = -1
+        else:
+            y_increment = 1
+
+        if dx > dy:
+            p = 2*dy - dx
+            c1 = 2*dy
+            c2 = 2*(dy-dx)
+
+            for i in range(dx):
+                x = x + x_increment
+                if p < 0:
+                    p = p + c1
+                else:
+                    p = p + c2
+                    y = y + y_increment
+
+                self.canvas.create_rectangle(x, y, x+1, y+1)
+        else:
+            p = 2*dx - dy
+            c1 = 2*dx
+            c2 = 2*(dx-dy)
+
+            for i in range(dx):
+                y = y + y_increment
+                if p < 0:
+                    p = p + c1
+                else:
+                    p = p + c2
+                    x = x + x_increment
+
+                self.canvas.create_rectangle(x, y, x+1, y+1)
+
+    def draw_parametric(self):
+        pass
+
+    # ---------- DRAW CIRCUMFERENCE ----------
+ 
+    def draw_circle(self):
+        
+        cx = int(self.x1_value.get())
+        cy = int(self.y1_value.get())
+        x = int(self.x2_value.get())
+        y = int(self.y2_value.get()) 
+        radius = sqrt(pow((x - cx), 2) + pow((y - cy), 2))
+
+        self.object_lst = [x1, y1, x2, y2]
+
+        x = 0
+        y = int(radius)
+        p = 3 - 2*radius
+        self.draw_symmetric(x, y, cx, cy)
+
+        while x < y:
+            if p < 0:
+                p += 4 * x + 6
+            else:
+                p += 4 * (x - y) + 10
+                y = y - 1
+            x = x + 1
+            self.draw_symmetric(x, y, cx, cy)
+
+    def draw_symmetric(self, x, y, cx, cy):
+
+        self.canvas.create_rectangle((cx + x), (cy + y), (cx + x)+1, (cy + y)+1)
+        self.canvas.create_rectangle((cx + x), (cy - y), (cx + x)+1, (cy - y)+1)
+        self.canvas.create_rectangle((cx - x), (cy + y), (cx - x)+1, (cy + y)+1)
+        self.canvas.create_rectangle((cx - x), (cy - y), (cx - x)+1, (cy - y)+1)
+        self.canvas.create_rectangle((cx + y), (cy + x), (cx + y)+1, (cy + x)+1)
+        self.canvas.create_rectangle((cx + y), (cy - x), (cx + y)+1, (cy - x)+1)
+        self.canvas.create_rectangle((cx - y), (cy + x), (cx - y)+1, (cy + x)+1)
+        self.canvas.create_rectangle((cx - y), (cy - x), (cx - y)+1, (cy - x)+1)
+
+    # ---------- MODIFICATIONS ----------
+ 
+    def translate(self):
+        self.canvas.delete(ALL)
+
+    def scale(self):
+        self.canvas.delete(ALL)
+
+    def rotate(self):
+        self.canvas.delete(ALL)
+
+    def crop(self):
+        pass
+
+    # ---------- CLEAR CANVAS ----------
+ 
+    def clear_canvas(self):
+        self.object_lst = []
+        self.canvas.delete(ALL)
+
+
+    def __init__(self):
+
+        # ---------- SETTING VARIABLES ----------
+
+        win_width = 800
+        win_height = 630
+        canvas_width = 700
+        canvas_height = 500
+
+        self.object_lst = []
+
+        # ---------- SETTING WINDOW ----------
+
+        window = Tk()
+        window.title("Computer Graphics App")
+        window.resizable(width=False, height=False)
+        window.geometry('{}x{}'.format(win_width, win_height))
+
+        # ---------- SETTING CANVAS AND IMAGE ----------
+
+        self.canvas = Canvas(window, width=canvas_width, height=canvas_height, bg="#ffffff")
+        self.canvas.pack()
+
+        frame = Frame(window)
+        frame.pack()
+
+        # ---------- TEXT INPUTS ----------
+
+        label_one = Label(frame, text="Insira os valores de x1 e y1: ").grid(row = 1, column = 2)
+        self.x1_value = StringVar()
+        Entry(frame, textvariable = self.x1_value).grid(row = 1, column = 3)
+        self.y1_value = StringVar()
+        Entry(frame, textvariable = self.y1_value).grid(row = 1, column = 4)
+
+        label_two = Label(frame, text="Insira os valores de x2 e y2: ").grid(row = 2, column = 2)
+        self.x2_value = StringVar()
+        Entry(frame, textvariable = self.x2_value).grid(row = 2, column = 3)
+        self.y2_value = StringVar()
+        Entry(frame, textvariable = self.y2_value).grid(row = 2, column = 4)
+
+        # ---------- ACTIONS FOR BUTTONS ----------
+
+        dda_button = Button(frame, text="DDA", command = self.draw_dda)
+        dda_button.grid(row = 3, column = 1)
+        
+        bresenham_button = Button(frame, text="Bresenham", command = self.draw_bresenham)
+        bresenham_button.grid(row = 3, column = 2)
+        
+        parametric_button = Button(frame, text="Equação Paramétrica", command = self.draw_parametric)
+        parametric_button.grid(row = 3, column = 3)
+        
+        circumference_button = Button(frame, text="Circunferência", command = self.draw_circle)
+        circumference_button.grid(row = 3, column = 4)
+
+        clear_button = Button(frame, text="Apagar", command = self.clear_canvas)
+        clear_button.grid(row = 3, column = 5)
+
+        crop_button = Button(frame, text="Recorte", command = self.crop)
+        crop_button.grid(row = 4, column = 1)
+
+        translation_button = Button(frame, text="Translação", command = self.translate)
+        translation_button.grid(row = 4, column = 2)
+        
+        scale_button = Button(frame, text="Escala", command = self.scale)
+        scale_button.grid(row = 4, column = 3)
+        
+        rotation_button = Button(frame, text="Rotação", command = self.rotate)
+        rotation_button.grid(row = 4, column = 4)
+
+        # ---------- EVENT LOOP ----------
+
+        window.mainloop()
+
+cg_app = graphics_app()
